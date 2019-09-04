f2968642e77014dd2067545c2826a03f4d393ad0
wubin <wubin@myreal3d.com>
2019-09-04 11:59:29 +0800

    ServiceProvider add configure support

--- ServiceProvider.php
+++ ServiceProvider.php
@@ -23,8 +23,27 @@ abstract class ServiceProvider implements Provider
         $this->container = $container;
     }
 
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
     public function register()
     {
         //
+    }
+
+    /**
+     * Merge the given configuration with the existing configuration.
+     *
+     * @param  string  $path
+     * @param  string  $key
+     * @return void
+     */
+    protected function mergeConfigFrom($path, $key)
+    {
+        $config = $this->container['config']->get($key, []);
+
+        $this->container['config']->set($key, array_merge(require $path, $config));
     }
 }
